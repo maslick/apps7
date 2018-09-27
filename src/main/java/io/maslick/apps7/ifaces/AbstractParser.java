d67313f87aef89c5d60fe20194e12bdd83e4847c
Pavel Maslov <pavel.masloff@gmail.com>
2018-09-27 08:59:55 +0200

    substituted ILineParser with Function

--- src/main/java/io/maslick/apps7/ifaces/AbstractParser.java
+++ src/main/java/io/maslick/apps7/ifaces/AbstractParser.java
@@ -7,12 +7,13 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class AbstractParser implements IParser {
     private final String COMMA = ",";
 
-    protected ILineParser lambda;
+    protected Function<String[], Data> lambda;
 
     @Override
     public List<Data> invoke(String input) {
@@ -23,7 +24,7 @@ public class AbstractParser implements IParser {
             InputStream inputFS = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8.name()));
             BufferedReader br = new BufferedReader(new InputStreamReader(inputFS));
 
-            list = br.lines().skip(1).map(line -> lambda.invoke(line.split(COMMA)))
+            list = br.lines().skip(1).map(line -> lambda.apply(line.split(COMMA)))
                     .filter(Objects::nonNull)
                     .collect(Collectors.toList());
 
--- src/main/java/io/maslick/apps7/ifaces/ILineParser.java
+++
@@ -1,7 +0,0 @@
-package io.maslick.apps7.ifaces;
-
-import io.maslick.apps7.pojo.Data;
-
-public interface ILineParser {
-    Data invoke(String[] line);
-}
